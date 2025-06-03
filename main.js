0196ddb8dde64be4ec21addccdbf4ca74894d387
Oussama Moutafatin <oussama@moutafatin.com>
2025-06-03 18:17:08 +0100

    Add rounds to the game

--- main.js
+++ main.js
@@ -1,36 +1,48 @@
-// "use strict";
+"use strict";
 
-function getComputerMove() {
-  const moves = ["rock", "paper", "scissors"];
-  return moves[Math.floor(Math.random() * 3)];
-}
+let movesLeft = 5;
+const movesLeftScore = document.querySelector(".moves-left-score");
+const userScore = document.querySelector(".user-score");
+const computerScore = document.querySelector(".computer-score");
+const roundMessage = document.querySelector(".round-msg");
 
-const gameAction = document.querySelectorAll(".game-action");
+const gameAction = document.querySelectorAll(".game-move");
 gameAction.forEach((action) => {
   action.addEventListener("click", (e) => {
     const userMove = e.currentTarget.dataset.moveName;
     const computerMove = getComputerMove();
 
     playRound(userMove, computerMove);
+    movesLeft--;
+    movesLeftScore.textContent = movesLeft;
+    if (movesLeft === 0) {
+      roundMessage.textContent = `Your total score is ${userScore.textContent}. You win/lose!`;
+      return;
+    }
   });
 });
 
+function getComputerMove() {
+  const moves = ["rock", "paper", "scissors"];
+  return moves[Math.floor(Math.random() * 3)];
+}
+
 function playRound(userMove, computerMove) {
-  const userScore = document.querySelector(".user-score");
-  const computerScore = document.querySelector(".computer-score");
-  const roundMessage = document.querySelector(".round-msg");
-  roundMessage.style.display = "block";
   if (userMove === computerMove) {
-    roundMessage.textContent = `A tie`;
+    showMessage(userMove, computerMove, "A tie!");
   } else if (
     (userMove === "paper" && computerMove == "rock") ||
     (userMove === "scissors" && computerMove === "paper") ||
     (userMove === "rock" && computerMove === "scissors")
   ) {
-    roundMessage.textContent = `You picked ${userMove} and the computer ${computerMove}. You win`;
+    showMessage(userMove, computerMove, "You win!");
     userScore.textContent = parseInt(userScore.textContent) + 1;
   } else {
-    roundMessage.textContent = `You picked ${userMove} and the computer ${computerMove}. You lose`;
+    showMessage(userMove, computerMove, "You lose!");
     computerScore.textContent = parseInt(computerScore.textContent) + 1;
   }
+}
+
+function showMessage(userMove, computerMove, message) {
+  roundMessage.textContent = `You picked ${userMove} and the computer ${computerMove}. ${message}`;
 }
