db7ae57f47e4226c6b92e46c0cd85608e2a85791
Oussama Moutafatin <oussama@moutafatin.com>
2025-06-03 19:06:56 +0100

    Doing a lot an forget to commit

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
 "use strict";
+const MAX_MOVES = 5;
 
-let movesLeft = 5;
+let movesLeft = MAX_MOVES;
 const movesLeftScore = document.querySelector(".moves-left-score");
 const userScore = document.querySelector(".user-score");
 const computerScore = document.querySelector(".computer-score");
 const roundMessage = document.querySelector(".round-msg");
 
+const resetGameBtn = document.querySelector(".reset-game");
+
+resetGameBtn.addEventListener("click", reset);
+
 const gameAction = document.querySelectorAll(".game-move");
 gameAction.forEach((action) => {
-  action.addEventListener("click", (e) => {
-    const userMove = e.currentTarget.dataset.moveName;
-    const computerMove = getComputerMove();
-
-    playRound(userMove, computerMove);
-    movesLeft--;
-    movesLeftScore.textContent = movesLeft;
-    if (movesLeft === 0) {
-      roundMessage.textContent = `Your total score is ${userScore.textContent}. You win/lose!`;
-      return;
-    }
-  });
+  action.addEventListener("click", handleUserPick);
 });
+
+function handleUserPick(e) {
+  if (movesLeft <= 0) {
+    return;
+  }
+  const userMove = e.currentTarget.dataset.moveName;
+  const computerMove = getComputerMove();
+
+  playRound(userMove, computerMove);
+
+  if (movesLeft === 0) {
+    endGame();
+    return;
+  }
+}
+
+function incrementScore(scoreElement) {
+  scoreElement.textContent = +scoreElement.textContent + 1;
+}
 
 function getComputerMove() {
   const moves = ["rock", "paper", "scissors"];
@@ -29,20 +42,61 @@ function getComputerMove() {
 
 function playRound(userMove, computerMove) {
   if (userMove === computerMove) {
-    showMessage(userMove, computerMove, "A tie!");
+    showRoundMessage(userMove, computerMove, "A tie!");
   } else if (
-    (userMove === "paper" && computerMove == "rock") ||
+    (userMove === "paper" && computerMove === "rock") ||
     (userMove === "scissors" && computerMove === "paper") ||
     (userMove === "rock" && computerMove === "scissors")
   ) {
-    showMessage(userMove, computerMove, "You win!");
-    userScore.textContent = parseInt(userScore.textContent) + 1;
+    showRoundMessage(userMove, computerMove, "You win!");
+    incrementScore(userScore);
   } else {
-    showMessage(userMove, computerMove, "You lose!");
-    computerScore.textContent = parseInt(computerScore.textContent) + 1;
+    showRoundMessage(userMove, computerMove, "You lose!");
+    incrementScore(computerScore);
   }
+  updateMovesLeft();
 }
 
-function showMessage(userMove, computerMove, message) {
-  roundMessage.textContent = `You picked ${userMove} and the computer ${computerMove}. ${message}`;
+function endGame() {
+  const playerScore = +userScore.textContent;
+  const compScore = +computerScore.textContent;
+
+  if (playerScore > compScore) {
+    showEndGameMessage(playerScore, compScore, "You win");
+  } else if (playerScore < compScore) {
+    showEndGameMessage(playerScore, compScore, "You lose");
+  } else {
+    showEndGameMessage(playerScore, compScore, "A tie");
+  }
+
+  setGameActionsEnabled(false);
+}
+
+function setGameActionsEnabled(enabled) {
+  gameAction.forEach((btn) => (btn.disabled = !enabled));
+}
+
+function updateMovesLeft() {
+  movesLeft--;
+  movesLeftScore.textContent = movesLeft;
+}
+
+function showEndGameMessage(playerScore, compScore, message) {
+  roundMessage.textContent = `Game over! Your final score is ${playerScore} | Computer score is ${compScore}. ${message}!`;
+}
+
+function showRoundMessage(userMove, computerMove, message) {
+  roundMessage.textContent = `You picked ${userMove}, computer picked ${computerMove}. ${message}`;
+}
+
+function reset() {
+  setGameActionsEnabled(true);
+
+  movesLeft = MAX_MOVES;
+  movesLeftScore.textContent = movesLeft;
+
+  userScore.textContent = 0;
+  computerScore.textContent = 0;
+
+  roundMessage.textContent = "Make your move!";
 }
